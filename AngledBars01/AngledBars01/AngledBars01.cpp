3ec97321148e6568fc4359701e8f05c148f39a1c
marcinkociolek <marcin.kociolek@gmail.com>
2016-10-11 14:01:12 -0400

    Create Uniform Roi - cricular roi added

--- AngledBars01/AngledBars01/AngledBars01.cpp
+++ AngledBars01/AngledBars01/AngledBars01.cpp
@@ -18,17 +18,17 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	std::string FileName, FolderName, FileNameBase, FileNameExtension;
-	FolderName = "C:\\Data\\ExtensiveDirAvgTest\\InputData2\\BarsA0-90F08T04\\"; // folder where to save outpute test images (use double slashes, even at the end)
+	FolderName = "C:\\Data\\ExtensiveDirAvgTest\\InputData3\\BarsA0-90F08T04\\"; // folder where to save outpute test images (use double slashes, even at the end)
 	FileNameBase = "Bars"; // beginning of test file name
 	FileNameExtension = ".tif"; // test file name extension
 
 	bool saveResult = 1; // 1 --> files saved
 	bool displayResult = 1; // 1 --> files only displayed
 	// only one of the following three options should be chosen, or none (type of noise)
-	bool preprocessBlurr = 1;
+	bool preprocessBlurr = 0;
 	bool addNoise = 0;
 	bool gausBlur = 0; // blur kernel if going to have a Gaussian shape (opencv function below, parameter fixed for now)
-	bool averageBlur = 0; // blur kernel if going to have constant value (1/number of pixels in the kernel)
+	bool averageBlur = 1; // blur kernel if going to have constant value (1/number of pixels in the kernel)
 	
 	int barTickness = 4;
 	int barFrequency = 8; // distance between corresponding bar location (i.e., barFrequency - barTickness = distance between bars)
@@ -47,8 +47,8 @@ int main(int argc, char* argv[])
 
 	bool cropImage = 1; // Crop image after rotation: 1-->yes; 0-->no
 
-	int iterNr = 10;//10; // number of modifed images (blur or noise): 0 means not distortion applied
-	int iterStart = 10;
+	int iterNr = 1;//10; // number of modifed images (blur or noise): 0 means not distortion applied
+	int iterStart = 1;
 
 	// the resulting image size if always half of these values, if cropping is on (see above)
 	int maxX = 1024;
---
+++ CreateUniformRoi/CreateUniformRoi/CreateUniformRoi.cpp
@@ -0,0 +1,115 @@
+// CreateUniformRoi.cpp : Defines the entry point for the console application.
+//
+
+#include "opencv2\core\core.hpp"
+#include "opencv2\highgui\highgui.hpp"
+#include "opencv2\imgproc\imgproc.hpp"
+
+#include <boost\filesystem.hpp>
+#include <boost/regex.hpp>
+
+#include <iostream>
+#include <fstream>
+
+
+#include "..\..\..\ProjectsLib\LibMarcin\Functions.h"
+#include "..\..\..\ProjectsLib\LibMarcin\NormalizationLib.h"
+#include "..\..\..\ProjectsLib\LibMarcin\HaralickLib.h"
+#include "..\..\..\ProjectsLib\LibMarcin\RegionU16Lib.h"
+
+#include "..\..\..\ProjectsLib\LibMarcin\ParamFromXML.h"
+#include "..\..\..\ProjectsLib\LibMarcin\DispLib.h"
+#include "..\..\..\ProjectsLib\LibMarcin\StringFcLib.h"
+
+//#include <tinyxml.h>
+//#include <tinystr.h>
+
+#define PI 3.14159265
+
+using namespace cv;
+using namespace std;
+using namespace boost;
+using namespace boost::filesystem;
+
+
+//const int stepNr = 180;
+
+
+int main(int argc, char* argv[])
+{
+	std::string FileName, FolderName, FileNameBase, FileNameExtension;
+	FolderName = "C:\\Data\\GroundTrueForTextFeat\\ROIs512x512c51x51xCount361\\"; // folder where to save outpute test images (use double slashes, even at the end)
+	FileNameBase = "ROI"; // beginning of test file name
+	FileNameExtension = ".tif"; // test file name extension
+
+	bool saveResult = 1; // 1 --> files saved
+	bool displayResult = 1;
+
+	int roiShape = 1;
+	// the resulting image size if always half of these values, if cropping is on (see above)
+	int maxX = 512;
+	int maxY = 512;
+
+	int roiSizeX = 51;
+	int roiSizeY = 51;
+
+	int roiOffsetX = 26;
+	int roiOffsetY = 26;
+
+	int roiStepX = 51;
+	int roiStepY = 51;
+
+	int roiCount = 361;
+
+	unsigned short roiIndex = 1;
+
+	Mat ROI = Mat::zeros(maxX, maxY, CV_16U);
+	for (int y = roiOffsetY; y < maxY - roiSizeY/2; y += roiStepY)
+	{
+		switch (roiShape)
+		{
+		case 1:
+			for (int x = roiOffsetX; x < maxX - roiSizeX / 2; x += roiStepX)
+			{
+				Point PointCenter(x, y);
+				ellipse(ROI, PointCenter, Size(roiSizeX / 2, roiSizeY / 2), 0.0, 0.0, 360.0, roiIndex, -1);
+
+				roiIndex++;
+			}
+			break;
+		default:
+			for (int x = roiOffsetX; x < maxX - roiSizeX / 2; x += roiStepX)
+			{
+				Point PointStart(x - roiSizeX / 2, y - roiSizeY / 2);
+				Point PointStop(x - roiSizeX / 2 + roiSizeX - 1, y - roiSizeY / 2 + roiSizeY - 1);
+
+				rectangle(ROI, PointStart, PointStop, roiIndex, -1);
+
+				roiIndex++;
+			}
+			break;
+		}
+	}
+
+	imshow("Image", ShowRegion(ROI));
+
+	waitKey(0);
+
+	for (int i = 0; i < roiCount; i++)
+	{
+		FileName = FolderName;
+		FileName += FileNameBase;
+		FileName += "Size";
+		FileName += to_string(roiSizeY);
+		FileName += "x";
+		FileName += to_string(roiSizeY);
+		FileName += "Nr";
+		FileName += ItoStrLZ(i, 3);
+		FileName += FileNameExtension;
+
+		imwrite(FileName, ROI);
+
+	}
+
+}
+
